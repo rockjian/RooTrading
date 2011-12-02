8196d08cdb01ba9f9020f1303b41ea4c83ce2981
rockjian <rockjian@gmail.com>
2011-12-02 08:13:18 -0500

    Update MarketData.r

--- MarketData.r
+++ MarketData.r
@@ -1,12 +1,14 @@
 #-- connect to SQLite DB
-setwd("D:/Jhuang/R/RTrading/")
+setwd("H:/Futures/R/")
 library(RSQLite)
 drv <- dbDriver("SQLite")
 con <- dbConnect(drv, dbname="market.db")
 
-#-- connect to Bloomberg
+#-- connect to Bloomberg 
 library(RBloomberg)
 conn <- blpConnect()
+
+library(quantmod)
 
 #-- Get SPX Mini contract intraday price data, up to 195 days
 ESID <- bar(conn, "ES1 Index", "TRADE", "2011-05-18 08:00:00.000", "2011-11-29 18:00:00.000", "5")
@@ -26,4 +28,27 @@ dbWriteTable(con, "SPD", SPD, overwrite=TRUE)
 #-- Get SPX index data, starting from 1957
 SPX <- bdh(conn, "SPX Index", c("PX_OPEN", "PX_HIGH", "PX_LOW", "PX_LAST", "VOLUME"),  "19570401", "20111129")
 dbWriteTable(con, "SPX", SPX, overwrite=TRUE)
+
+#-- Bloomberg data is of data.frame format. We need to transfer it into xts class.
+
+-- remove "T" character
+ESID[,1] <- sub("T", " ", ESID[,1])
+-- covert timestamp
+ESID[,1] <- as.POSIXct(ESID[,1], format="%Y-%m-%d %H:%M:%S")
+-- change to EST time zone
+ESID[,1] <- ESID[,1] - 3600*5
+-- discard tick counts
+ESID$numEvents <- NULL
+-- change to xts type
+ESID <- xts(ESID[,-1], order.by=ESID[,1])
+
+
+-- OK, we get intraday chart
+chartSeries(ESID, major.ticks="minutes")
+
+
+
+
+
+
 
